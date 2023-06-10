a01f2f8e46a2d4be84cd788ccb34c192e8155f13
stonehank <stonehank310@gmail.com>
2023-06-10 04:32:42 +0000

    Update

--- static/1686370211/projects/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fstonehank.github.io\u002Fstatic\u002F1686370211",layout:"default",error:null,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fstonehank.github.io\u002F"}}}}("\u002F"));
